03107084507b02a3d92012b60a279fe7938011d2
juzisang <juziexe@gmail.com>
2018-11-06 11:15:37 +0800

    feat(lib): add express

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+export default {
+  serve: {
+    api: "http://localhost:23333",
+    port: 23333
+  }
+};
--- src/main/index.ts
+++ src/main/index.ts
@@ -1,10 +1,9 @@
-"use strict";
-
 import { app, protocol, BrowserWindow } from "electron";
 import {
   createProtocol,
   installVueDevtools
 } from "vue-cli-plugin-electron-builder/lib";
+import "./app";
 const isDevelopment = process.env.NODE_ENV !== "production";
 
 // Keep a global reference of the window object, if you don't, the window will
@@ -15,7 +14,7 @@ let win: any;
 protocol.registerStandardSchemes(["app"], { secure: true });
 function createWindow() {
   // Create the browser window.
-  win = new BrowserWindow({ width: 800, height: 600 });
+  win = new BrowserWindow({ width: 800, height: 600, frame: false });
 
   if (isDevelopment) {
     // Load the url of the dev server if in development mode
